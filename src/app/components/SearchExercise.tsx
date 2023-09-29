8f1d4befc665db987e3f620bb8ca7dd7c6a6a3a3
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-09-29 20:48:20 +0200

    style changes

--- src/app/components/SearchExercise.tsx
+++ src/app/components/SearchExercise.tsx
@@ -58,10 +58,10 @@ export default function SearchExercise({setUrlMuscle, setUrlEquipment, fetchResu
 
     // make a component to go in the placeholders, that contains all the muscles and equipment that can be selected 
     return (
-        <div id="menu-container" className={`grid grid-flow-row grid-cols-2 tracking-wider text-xl p-8 w-full font-d justify-stretch ${dela.className}`}>
-            <div id="muscle" className="p-4 hover:bg-slate-500 cursor-pointer auto-rows-max auto-cols-max min-h-0 min-w-0" onClick={handleClickM}>Muscle</div>
+        <div id="menu-container" className={`grid grid-flow-row grid-cols-2 tracking-wider text-xl p-8 font-d justify-stretch ${dela.className}`}>
+            <div id="muscle" className="p-4 hover:bg-slate-500 cursor-pointer auto-rows-max auto-cols-max min-h-0  " onClick={handleClickM}>Muscle</div>
 
-            <div id="equipment" className=" p-4 hover:bg-slate-500 cursor-pointer min-h-0 min-w-0" onClick={handleClickE}>Equipment
+            <div id="equipment" className=" p-4 hover:bg-slate-500 cursor-pointer min-h-0 " onClick={handleClickE}>Equipment
             </div>
             <div id="top-right-containter" className="row-start-1 col-start-2 min-h-0 min-w-0"><Muscles /></div>
             <div id="bottom-right-containter" className="row-start-2 col-start-2 min-h-0 min-w-0 "><Equipment /></div>
