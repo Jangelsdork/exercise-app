0a2fb61955049fc8e287b1f16ed96d83b0376f16
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-09-29 07:51:30 +0200

    change font size

--- src/app/components/Exercises.tsx
+++ src/app/components/Exercises.tsx
@@ -16,7 +16,7 @@ export default function Exercises({apiRes}:Props) {
             return(
                 <div key={index} className="">
                 <ul  className="grid grid-cols-2 m-2 border-[1px] border-slate-600 p-2">
-                    <li className={`tracking-wider ${dela.className}`}>{line.name}</li>
+                    <li className={`tracking-wider text-xl ${dela.className}`}>{line.name}</li>
                     <li className="font-thin m-2">{desc}</li>
                 </ul>
                 </div>
