90c0f91ec3bcf50a26f308494cdb6491007ee9fb
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2023-09-29 17:26:00 +0200

    playing with ui for client inputs

--- src/app/components/SearchExercise.tsx
+++ src/app/components/SearchExercise.tsx
@@ -5,8 +5,11 @@ export default function SearchExercise() {
     const [muscleClicked, setMuscleClicked] = useState<boolean>(false)
     const [equipmentClicked, setEquipmentClicked] = useState<boolean>(false)
 
-    function handleClick(){
+    function handleClickM(){
         setMuscleClicked(!muscleClicked)
+    }
+    function handleClickE(){
+        setEquipmentClicked(!equipmentClicked)
     }
     
     function Muscles(){
@@ -19,18 +22,29 @@ export default function SearchExercise() {
                 </div>
             )
         }
+    }
+    function Equipment(){
+        if(equipmentClicked===true){
+            return(
+                <div>
+                <div className="p-4 hover:bg-slate-500">Dumbbells</div>
+                <div className="p-4 hover:bg-slate-500">Barbell</div>
+                <div className="p-4 hover:bg-slate-500">Bodyweight</div>
+                </div>
+            )
+        }
 
     }
 
     // make a component to go in the placeholders, that contains all the muscles and equipment that can be selected 
     return (
-        <div id="menu-container" className={`grid grid-flow-col grid-rows-2 grid-cols-2 tracking-wider text-xl p-8 w-full font-d ${dela.className}`}>
-            <div id="muscle" className="border-[1px] border-slate-600 p-4 hover:bg-slate-500 cursor-pointer" onClick={handleClick}>Muscle</div>
+        <div id="menu-container" className={`grid grid-flow-col grid-rows-2 grid-cols-2 tracking-wider text-xl p-8 w-full font-d justify-stretch ${dela.className}`}>
+            <div id="muscle" className="p-4 hover:bg-slate-500 cursor-pointer auto-rows-max auto-cols-max min-h-0 min-w-0" onClick={handleClickM}>Muscle</div>
 
-            <div id="equipment" className="border-[1px] border-slate-600 p-4 hover:bg-slate-500">Equipment
+            <div id="equipment" className=" p-4 hover:bg-slate-500 cursor-pointer min-h-0 min-w-0" onClick={handleClickE}>Equipment
             </div>
-            <div id="top-right-containter" className="row-start-1 col-start-2 "><Muscles /></div>
-            <div id="top-right-containter" className="row-start-2 col-start-2 ">placeholder 2</div>
+            <div id="top-right-containter" className="row-start-1 col-start-2 min-h-0 min-w-0"><Muscles /></div>
+            <div id="bottom-right-containter" className="row-start-2 col-start-2 min-h-0 min-w-0 "><Equipment /></div>
         </div>
     )
 }
